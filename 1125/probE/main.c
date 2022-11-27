733aa3ed01721e154b391f7a6f437591f615c143
RobbyRux <deangelis.robert13@gmail.com>
2022-11-27 17:01:25 -0500

    need to fix pointers

--- 1125/probE/main.c
+++ 1125/probE/main.c
@@ -2,47 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
+int compare(const void * a, const void * b) {
+    int (*int_a)[2] = (int (*)[2]) a;
+    int (*int_b)[2] = (int (*)[2]) b;
+
+    printf("%d, %d \n", int_a[0],int_a[1]);
+
+    if ( int_a[0] == int_b[0] ) {
+        if ( int_a[1] == int_b[1] ) return 0;
+        else if ( int_a[1] < int_b[1] ) return -1;
+        else return 1;
+    }
+    else if ( abs(*int_a[0]-12) < abs(*int_b[0]-12) ) return -1;
+    else return 1;
+}
 
 int main(){
     int times=5;
     scanf("%d", &times);
     while(times){
-        int sortedAM[100][2];
+        int sortedAM[times][2];
         int amIndex=0;
-        int sortedPM[100][2];
+        int sortedPM[times][2];
         int pmIndex=0;
         for(int i=0; i<times;i++) {
-            char input[10];
-            scanf("%s", input);
-            char *ptr = strtok(input, " ");
-            char part1[5];
-            strcpy(part1,ptr);
-            ptr = strtok(NULL, " ");
-            char part2[5];
-            strcpy(part2,*ptr);
-            printf("First: %s\t Second: %s\n",part1,part2);
-        }
-        scanf("%d",&times);
-    }
-    /*while(times){
-        int sortedAM[100][2];
-        int amIndex=0;
-        int sortedPM[100][2];
-        int pmIndex=0;
-        for(int i =0; i < times;i++){
-            char input[11];
-            fgets(input,10,stdin);
-            char *ptr = strtok(input, " ");
-            if(strcmp(strtok(NULL, " "), "a.m.")) {
-                sortedAM[amIndex][0]=atoi(strtok(ptr,":"));
-                sortedAM[amIndex++][1]=atoi(strtok(NULL,":"));
+            char cat[5];
+            int nums[2];
+            scanf("%d:%d %s", &nums[0],&nums[1], cat);
+            if(strcmp(cat, "a.m.")){
+                sortedAM[amIndex][0]=nums[0];
+                sortedAM[amIndex++][1]=nums[1];
+            } else {
+                sortedPM[pmIndex][0]=nums[0];
+                sortedPM[pmIndex++][1]=nums[1];
             }
         }
-        for(int i=0; i < amIndex; i++){
-            printf("%d %d\n", sortedAM[i][0],sortedAM[i][1]);
-        }
-        scanf("%d", &times);
-    }*/
+    compare(sortedAM[1], sortedAM[0]);
+    scanf("%d",&times);
+    }
     return 0;
 }
 
